eba37b18a388fdedc809da2ad13d19841cd7d041
clement <osternaud.clement@gmail.com>
2018-05-07 19:46:19 +0200

    Remove secret config

---
+++ src/config.default.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // API
+  api: {
+    ghost: {
+      url: 'GHOST_ADMIN_URL',
+      port: 'GHOST_ADMIN_PORT',
+      endpoint: 'GHOST_API_ENDPOINT',
+      clientId: 'ghost-frontend',
+      clientSecret: 'GHOST_CLIENT_SECRET',
+    },
+  },
+};
